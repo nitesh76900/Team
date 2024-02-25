ff7ef25c72be0ef4a7995ade9a69f1503b8f8e1f
nitesh76900 <145578270+nitesh76900@users.noreply.github.com>
2024-02-25 09:35:03 +0530

    Initial commit

---
+++ web file/mail.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGnqvPWGDCKuf8zTGnfBaFjCYHK7KHlGk",
+    authDomain: "fir-database-e2334.firebaseapp.com",
+    databaseURL: "https://fir-database-e2334-default-rtdb.firebaseio.com",
+    projectId: "fir-database-e2334",
+    storageBucket: "fir-database-e2334.appspot.com",
+    messagingSenderId: "1041126756829",
+    appId: "1:1041126756829:web:8733818ee110518874e634"
+  };
+
+  firebase.initializeApp(firebaseConfig);
